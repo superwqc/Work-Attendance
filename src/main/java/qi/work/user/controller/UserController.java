5d6fe09a7028f13b3c209ac6b64156faed1bbf2e
superwqc <superwqc@163.com>
2017-09-02 22:24:51 +0800

    Attend

--- src/main/java/qi/work/user/controller/UserController.java
+++ src/main/java/qi/work/user/controller/UserController.java
@@ -3,8 +3,6 @@ package qi.work.user.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import qi.work.user.dao.UserMapper;
-import qi.work.user.entity.User;
 import qi.work.user.service.UserService;
 
 @Controller
@@ -18,13 +16,6 @@ public class UserController {
     @RequestMapping("/index")
     public String user(){
 
-        User u=new User();
-
-        u.setUname("man");
-        u.setPassword("123");
-        u.setRealname("老刘");
-        u.setMobile("134444444444");
-        userService.createUser(u);
 
         System.out.println("overgame");
         return "user";
