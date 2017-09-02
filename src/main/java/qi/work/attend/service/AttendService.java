ba944a1992db879c0e55db317ea1af87ee52134e
superwqc <superwqc@163.com>
2017-09-02 22:23:42 +0800

    change

---
+++ src/main/java/qi/work/attend/service/AttendService.java
@@ -0,0 +1,10 @@
+package qi.work.attend.service;
+
+import qi.work.attend.entity.Attend;
+
+/**
+ * Created by Qi on 2017/9/2.
+ */
+public interface AttendService {
+    void signAttend(Attend attend);
+}
---
+++ src/main/java/qi/work/attend/service/AttendServiceImpl.java
@@ -0,0 +1,13 @@
+package qi.work.attend.service;
+
+import qi.work.attend.entity.Attend;
+
+/**
+ * Created by Qi on 2017/9/2.
+ */
+public class AttendServiceImpl implements AttendService{
+    @Override
+    public void signAttend(Attend attend) {
+
+    }
+}
